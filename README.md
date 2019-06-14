6ce4dc4f7e9f467d496eff7f812b92e5020c088b
antoniovillegash <antonio.villegas.0101@gmail.com>
2019-06-14 00:01:06 -0500

    automating projects

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-ADD README CONTENT
+Automating projects with bash
+
+*setup*
+open the new-project.sh file
+and change the lines 3 & 4 with your corresponding path & github username
+
+*Run*
+in order to run the script you have to make:
+
+$cd into the directory where new-file.sh is located or add the .sh file into /usr/local/bin to use it from any directory.
+
+$chmod +x new-project.sh
+
+$./new-project.sh <programming languaje> <name-of-the-project>
+	
+<java> as -java
+<python> as -py
+
+*Automating*
+the automating project does:
+*makes the main project folder
+*creates the git repository and commits the README file
+*creates the src folder
+*creates a file inside the src folder	 
+	
+ 
---
+++ new-project.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+Path="/home/tony/Programas/" #change path to your projects folder
+GitUser="antoniovillegash" #set your github user
+Extension=""
+Folder=""
+case "$1" in
+"-py")
+    Extension="py"
+    Folder="python"
+    ;;
+"-java")
+    Extension="txt"
+    Folder="java"
+    ;;
+*)
+    echo $1" is not a valid cammand try -py or -java"
+    exit 0
+    ;;
+esac
+if [ ! -d $Path$Folder ]; then
+    cd $Path
+    mkdir -p $Folder
+fi;
+cd $Path$Folder/
+if [ ! -d $2 ]; then
+    echo "creating proyect: " $2
+    mkdir -p $2
+    echo "created main folder: " $2
+    cd $2
+    curl -u $GitUser https://api.github.com/user/repos -d "{\"name\":\"$2\"}"
+    git init
+    echo "ADD README CONTENT" > README.md
+    git add README.md
+    git commit -m "Initial"
+    git remote add origin https://github.com/$GitUser/$2.git
+    git push -u origin master
+    mkdir "src"
+    echo "created src folder: "
+    cd $Path$Folder/$2/src/
+    > $2.$Extension
+else
+    echo "A folder named "$2" already exist"
+fi;
